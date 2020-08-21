f264ef6b1b346a7ebca20fecce28a0854fbdbe53
Patrick Swaney <prswaney@gmail.com>
2020-08-21 14:17:53 -0700

    resize images, give them click functions, add unecissary submit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
   $("#formOne").submit(function (event) {
+    event.preventDefault();
+    // saving the value of the input for further use in the code
     const animalInput = $("input#animal").val().toLowerCase();
-
+    console.log(animalInput);
     if (animalInput === "turtle" || animalInput === "turtles") {
       $('#turtles').show();
     } else if (animalInput === "snake" || animalInput === "snakes") {
@@ -10,5 +12,32 @@ $(document).ready(function () {
       $('#insects').show();
     }
   })
-  $("#formTwo").click()
+  // function that handles a click on images
+  $("#green-sea-turtle").click(function () {
+    alert("Handler for .click() called.");
+  });
+  $("#african-spurred-tortoise").click(function () {
+    alert("Handler for .click() called.");
+  });
+  $("#red-eared-slider-terrapin").click(function () {
+    alert("Handler for .click() called.");
+  });
+  $("#corn-snake").click(function () {
+    alert("Handler for .click() called.");
+  });
+  $("#green-tree-python").click(function () {
+    alert("Handler for .click() called.");
+  });
+  $("#golden-lancehead").click(function () {
+    alert("Handler for .click() called.");
+  });
+  $("#fire-ants").click(function () {
+    alert("Handler for .click() called.");
+  });
+  $("#monarch-butterfly").click(function () {
+    alert("Handler for .click() called.");
+  });
+  $("#honey-bee").click(function () {
+    alert("Handler for .click() called.");
+  });
 })
